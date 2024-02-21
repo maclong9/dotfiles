df74c48e55d3de2ff9fd06848189db569d6819dc
Mac <maclong9@icloud.com>
2024-02-21 17:37:47 +0000

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -103,7 +103,7 @@ emacs_install() {
 
     git clone https://github.com/hlissner/doom-emacs "$HOME"/.emacs.d || error_clean "Error cloning doom-emacs to $HOME/.emacs.d"
     "$HOME"/.emacs.d/bin/doom install || error_clean "Error running doom install"
-    sips -i https://github.com/SavchenkoValeriy/emacs-icons/blob/main/curvy-blender/Emacs.icns /Applications/Emacs.app/Contents/Resources/AppIcon.icns || error_clean "Error changing Emacs icon"
+    sips -i https://github.com/SavchenkoValeriy/emacs-icons/blob/main/curvy-blender/Emacs.icns /Applications/MacPorts/EmacsMac.app/Contents/Resources/AppIcon.icns || error_clean "Error changing Emacs icon"
 
     success "Doom Emacs installed successfully"
 }
