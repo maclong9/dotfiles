abb0609a9e87df8afbdbe3d7926ef68c1427994b
Mac <maclong9@icloud.com>
2024-02-21 01:35:44 +0000

    Update init.e

--- doom/init.el
+++ doom/init.el
@@ -5,9 +5,6 @@
 
        :completion
        company           ; the ultimate code completion backend
-       ;;helm              ; the *other* search engine for love and life
-       ;;ido               ; the other *other* search engine...
-       ;;ivy               ; a search engine for love and life
        vertico           ; the search engine of the future
 
        :ui
