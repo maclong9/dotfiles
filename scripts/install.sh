19ce075bdcf4f32a7975740196f370eac1cd4e2d
Mac <maclong9@icloud.com>
2024-02-21 16:03:19 +0000

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -21,6 +21,18 @@ error_exit() {
 }
 
 error_clean() {
+    printf "\n\033[1;31m✘ %s, removing ~/.config and MacPorts then exiting.\033[0m\n" "$1"
+    sudo rm -rf ~/.config /opts/mports ~/.ssh
+    rm -rf ~/Downloads/Hyperkey0.28.dmg
+
+    if [ -d "/Volumes/Hyperkey0.28" ]; then
+        hdiutil detach /Volumes/Hyperkey0.28
+    fi
+    
+    exit 1
+}
+
+error_clean() {
     printf "\n\033[1;31m✘ %s, removing $HOME/.config and MacPorts then exiting.\033[0m\n" "$1"
     
     sudo rm -rf "$HOME"/.config /opts/mports "$HOME"/.ssh
