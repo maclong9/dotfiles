0a60e4c1cd92b0006196d4b0ccea2d7180e6becb
Mac <maclong9@icloud.com>
2024-02-21 20:50:00 +0000

    Update config.el

--- doom/config.el
+++ doom/config.el
@@ -1,8 +1,9 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
-(setq 
-  user-full-name "Mac" user-mail-address "maclong9@icloud.com"
-  doom-font (font-spec :family "SFMono Nerd Font" :size 12 :weight 'semi-light))
-  doom-theme 'doom-one)
-  display-line-numbers-type nil)
-  org-directory "/Users/mac/Library/Mobile Documents/com~apple~CloudDocs/org"
+(setq
+ user-full-name "Mac"
+ user-mail-address "maclong9@icloud.com"
+ doom-font (font-spec :family "SFMono Nerd Font" :size 12 :weight 'semi-light)
+ doom-theme 'doom-one
+ display-line-numbers-type nil
+ org-directory "/Users/mac/Library/Mobile Documents/com~apple~CloudDocs/org"
 )
