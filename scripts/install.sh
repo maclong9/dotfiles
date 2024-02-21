02adaea57cc7634fd9f5efde10649f957631fd0d
Mac <maclong9@icloud.com>
2024-02-21 16:52:30 +0000

    added echo

--- scripts/install.sh
+++ scripts/install.sh
@@ -28,20 +28,20 @@ error_clean() {
     if [ -d "/Volumes/Hyperkey0.28" ]; then
         hdiutil detach /Volumes/Hyperkey0.28
     fi
-    
+
     exit 1
 }
 
 error_clean() {
     printf "\n\033[1;31m✘ %s, removing $HOME/.config and MacPorts then exiting.\033[0m\n" "$1"
-    
+
     rm -rf "$HOME"/.config /opts/mports "$HOME"/.ssh
     rm -rf "$HOME"/Downloads/Hyperkey0.28.dmg
 
     if [ -d /Volumes/Hyperkey0.28 ]; then
         hdiutil detach /Volumes/Hyperkey0.28
     fi
-    
+
     exit 1
 }
 
@@ -51,7 +51,7 @@ clone_configuration() {
     if [ -d "$HOME"/.config ]; then
         error_exit "$HOME/.config already exists"
     else
-        git clone https://github.com/mac-codes9/dotfiles "$HOME"/.config 
+        git clone https://github.com/mac-codes9/dotfiles "$HOME"/.config
     fi
 
     success "Configuration cloned successfully"
@@ -69,13 +69,13 @@ ports_install() {
         make install
         make distclean
     fi
-    
+
     success "MacPorts is installed"
 }
 
 install_hyperkey() {
     message "Installing Hyperkey..."
-    
+
     curl -LO https://hyperkey.app/downloads/Hyperkey0.28.dmg
     hdiutil attach "$HOME"/Downloads/Hyperkey0.28.dmg
     cp -R /Volumes/Hyperkey0.28/Hyperkey.app /Applications
@@ -87,7 +87,7 @@ install_hyperkey() {
 
 install_sioyek() {
     message "Installing Sioyek..."
-    
+
     curl -LO https://github.com/ahrm/sioyek/releases/download/v2.0.0/sioyek-release-mac.zip
     unzip sioyek-release-mac.zip
     hdiutil attach "$HOME"/Downloads/build/sioyek.dmg
@@ -100,9 +100,9 @@ install_sioyek() {
 
 emacs_install() {
     message "Installing Doom Emacs..."
-    
+
     git clone https://github.com/hlissner/doom-emacs "$HOME"/.emacs.d || error_clean "Error cloning doom-emacs to $HOME/.emacs.d"
-    "$HOME"/.emacs.d/bin/doom install || error_clean "Error running doom install" 
+    "$HOME"/.emacs.d/bin/doom install || error_clean "Error running doom install"
     sips -i https://github.com/SavchenkoValeriy/emacs-icons/blob/main/curvy-blender/Emacs.icns /Applications/Emacs.app/Contents/Resources/AppIcon.icns || error_clean "Error changing Emacs icon"
 
     success "Doom Emacs installed successfully"
@@ -113,25 +113,26 @@ tooling_install() {
 
     echo 'export PATH="/opt/local/bin:$HOME/.bun/bin:$HOME/.emacs.d/bin:$PATH"' > ~/.profile
     source ~/.profile
+    echo $PATH
     port install "${@:tooling}" || error_clean "Error installing tooling with MacPorts"
     curl -fsSL https://bun.sh/install | bash || error_clean "Error installing bun"
     emacs_install || error_clean "Error installing Emacs"
-    
+
     success "Tooling has been installed successfully"
 }
 
 app_install() {
     message "Installing Applications with mas..."
-    
+
     mas install "${@:apps}" || error_clean "Error installing applications with mas"
     install_hyperkey || error_clean "Error installing Hyperkey"
-    
+
     success "Applications have been installed"
 }
 
 ssh_setup() {
   message "Configuring SSH..."
-  
+
   ssh-keygen -t ed25519 -C "maclong9@icloud.com" -f "$HOME"/.ssh/mac-mb
   eval "$(ssh-agent -s)"
   echo "
@@ -149,24 +150,24 @@ ssh_setup() {
 
 post_install() {
     message "Running post install setup..."
-    
+
     skhd --start-service
     yabai --start-service
     open -a /Applications/Hyperkey.app
     open -a /Applications/Linear.app
     git config --global user.email "maclong9@icloud.com" && git config --global user.name "Mac"
     ssh_setup
-    
+
     success "Post install setup complete."
 }
 
 main() {
     if [ "$(uname -s)" = "Darwin" ]; then
         message "􀣺 Running on macOS"
-        
+
         clone_configuration || error_clean "Failed to clone configuration repository"
         ports_install || error_clean "/opts/mports/macports-base does not exist"
-        tooling_install 
+        tooling_install
         app_install
         post_install || error_exit "Error running post installation setup"
 
