ecb683fc723e295d354a8815a1f059a8a2adeef3
Mac <maclong9@icloud.com>
2024-02-22 12:07:25 +0000

    Delete doom directory

--- doom/config.el
+++
@@ -1,9 +0,0 @@
-;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
-(setq
- user-full-name "Mac"
- user-mail-address "maclong9@icloud.com"
- doom-theme 'doom-one
- display-line-numbers-type nil
- fancy-splash-image (concat doom-private-dir "splash.png")
- doom-font (font-spec :family "Liga SFMono Nerd Font" :size 12 :weight 'medium)
-)
--- doom/packages.el
+++
@@ -1,2 +0,0 @@
-;; -*- no-byte-compile: t; -*-
-(package! leetcode :recipe (:host github :repo "kaiwk/leetcode.el"))
