da4f11e4161ff3f959ff4bb539763eb467c60e09
Mac <maclong9@icloud.com>
2024-02-22 12:15:15 +0000

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -36,62 +36,92 @@ clone_configuration() {
     success "Configuration cloned successfully"
 }
 
-install_bun() {
-    if ! command -v bun > /dev/null; then
-        curl -fsSL https://bun.sh/install | bash || error_clean "Error installing bun"
-    fi
-}
-
-install_doom() {
-    message "Installing Doom Emacs..."
-    
-    if [ ! -d "$HOME"/.emacs.d ]; then
-        git clone https://github.com/hlissner/doom-emacs "$HOME"/.emacs.d || error_clean "Error cloning doom-emacs to $HOME/.emacs.d"
-    fi
-    
-    "$HOME"/.emacs.d/bin/doom install || error_exit "Error running doom install"
-    
-    success "Doom Emacs installed successfully"
-}
-
-tooling_install() {
+brew_install() {
     message "Installing Hombrew"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     echo "
     eval "$(/opt/homebrew/bin/brew shellenv)"
-    export PATH="$PATH:Applications/Emacs.app/Contents/MacOS:/opt/homebrew/bin:~/.emacs.d/bin"
+    export PATH="$PATH:/opt/homebrew/bin"
     " > "$HOME"/.zprofile
-    mv /Applications/Emacs.app/Contents/MacOS/Emacs /Applications/Emacs.app/Contents/MacOS/emacs
     . "$HOME"/.zprofile
+    success "Homebrew installed successfully"
+}
+
+tooling_install() {
     message "Installing tooling with Brew..."
-    brew install homebrew/cask/emacs fd fzf gh hyperkey mas ripgrep rust sd zig || error_clean "Error installing tooling with Brew"
+
+    brew install fd fzf gh homebrew/cask/zed hyperkey mas ripgrep rust sd koekeishiya/formulae/skhd koekeishiya/formulae/yabai zig
     mas install 1436953057
-    install_bun || error_clean "Error installing bun from script"
-    install_doom || error_clean "Error installing Emacs"
+    install_doom
 
     success "Tooling has been installed successfully"
 }
 
 configure_git() {
     message "Configuring Git"
-    git config --global user.email "maclong9@icloud.com"
-    git config --global user.name "Mac"
-    git config --global push.default current
-    git config --global init.defaultBranch main
-    git config --global merge.conflictStyle zdiff3
-    git config --global help.autocorrect 30
-    git config --global rebase.autosquash true
+    
+    echo "
+    [user]
+	email = maclong9@icloud.com
+	name = Mac
+    [push]
+	default = current
+    [init]
+	defaultBranch = main
+    [merge]
+	conflictStyle = zdiff3
+    [help]
+	autocorrect = 30
+    [rebase]
+	autosquash = true
+    " > ~/.gitconfig
     gh config set git_protocol ssh
     gh auth login
+    
     success "Git configured successully"
   }
 
 post_install() {
-    ln -s ~/.config/yabairc ~/.yabairc 
-    ln -s ~/.config/skhdrc ~/.skhdrc
+    message "Configuring tools"
+    
+    echo "
+    yabai -m config layout managed
+    yabai -m config top_padding    10
+    yabai -m config left_padding   10
+    yabai -m config right_padding  10
+    yabai -m config window_gap     10
+    yabai -m config external_bar main:0:16
+    yabai -m config focus_follow_mouse autoraise
+    yabai -m rule --add app="^System Settings$" manage=off
+    yabai -m rule --add app="^Activity Monitor$" manage=off
+    yabai -m rule --add app="^App Store$" manage=off
+    yabai -m rule --add app="^Calendar$" manage=off
+    yabai -m rule --add app="^Mail$" manage=off
+    yabai -m rule --add app="^Reminders$" manage=off
+    " > ~/.yabairc 
+    
+    echo "
+    cmd - h : yabai -m window --focus west
+    cmd - j : yabai -m window --focus south
+    cmd - k : yabai -m window --focus north
+    cmd - l : yabai -m window --focus east
+    cmd + ctrl - h : yabai -m window --warp west
+    cmd + ctrl - j : yabai -m window --warp south
+    cmd + ctrl - k : yabai -m window --warp north
+    cmd + ctrl - l : yabai -m window --warp east
+    cmd + shift - h : yabai -m window --resize left:-15:0; yabai -m window --resize right:-15:0
+    cmd + shift - j : yabai -m window --resize bottom:0:15; yabai -m window --resize top:0:15
+    cmd + shift - k : yabai -m window --resize top:0:-15; yabai -m window --resize bottom:0:-15
+    cmd + shift - l : yabai -m window --resize right:15:0; yabai -m window --resize left:15:0
+    cmd + enter : open /Applications/Alacritty.app
+    cmd + b : open /Applications/Safari.app
+    " > ~/.skhdrc
+
     yabai --start-service
     skhd --start-service
-    configure_git || error_exit "Error configuring git"
+    configure_git
+
+    success "Post install configuration complete"
 }
 
 main() {
@@ -99,8 +129,9 @@ main() {
         message "􀣺 Running on macOS"
 
         clone_configuration || error_clean "Failed to clone configuration repository"
-        tooling_install
-        post_install
+        brew_install || error_clean "Failed to install Homebrew"
+        tooling_install || error_clean "Error while installing tooling with homebrew"
+        post_install || error_clean "Error during post install configuration"
 
         success "System configuration complete, enjoy."
     else
