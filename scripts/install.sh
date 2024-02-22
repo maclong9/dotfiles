9a1656736bfe69b19e4cf1d977d833eb6370feaa
Mac <maclong9@icloud.com>
2024-02-22 11:48:11 +0000

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -64,7 +64,7 @@ tooling_install() {
     mv /Applications/Emacs.app/Contents/MacOS/Emacs /Applications/Emacs.app/Contents/MacOS/emacs
     . "$HOME"/.zprofile
     message "Installing tooling with Brew..."
-    brew install homebrew/cask/emacs fd fzf gh hyperkey mas ripgrep rust sd koekeishiya/formulae/skhd koekeishiya/formulae/yabai zig || error_clean "Error installing tooling with Brew"
+    brew install homebrew/cask/emacs fd fzf gh hyperkey mas ripgrep rust sd zig || error_clean "Error installing tooling with Brew"
     mas install 1436953057
     install_bun || error_clean "Error installing bun from script"
     install_doom || error_clean "Error installing Emacs"
