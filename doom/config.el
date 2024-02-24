a4b74f0b3b1ce7635a2b4e19102c6296acd71f44
Mac <maclong9@icloud.com>
2024-02-24 11:27:28 +0000

    Create config.el

---
+++ doom/config.el
@@ -0,0 +1,8 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+(setq user-full-name "Mac"
+      user-mail-address "maclong9@icloud.com"
+      doom-font (font-spec :family "Liga SFMono Nerd Font" :size 12)
+      doom-theme 'doom-one
+      display-line-numbers-type "relative"
+      )
