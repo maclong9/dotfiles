54f95be4bf4a0fe75199469a0de9bd495fcaaf36
Mac <maclong9@icloud.com>
2024-02-26 22:02:52 +0000

    Create init.lua

---
+++ nvim/lua/user/init.lua
@@ -0,0 +1,15 @@
+return {
+  colorscheme = "nord",
+  lsp = {
+    formatting = {
+      format_on_save = true,
+    },
+  },
+  plugins = {
+    "AstroNvim/astrocommunity",
+    { import = "astrocommunity.colorscheme.nord-nvim"},
+    { import = "astrocommunity.git.neogit" },
+    { import = "astrocommunity.project.project-nvim" },
+    { import = "astrocommunity.motion.nvim-spider"},
+  },
+}
