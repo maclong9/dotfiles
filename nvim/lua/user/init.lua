ce7511ca982583e0aef60d915697a248b4bcb580
Mac <maclong9@icloud.com>
2024-02-29 12:35:58 +0000

    feat: colors, mappings and plugins
    - oxocarbon theme
    - added mappings for aerial and telescope projects
    - disabled notifiy

--- nvim/lua/user/init.lua
+++ nvim/lua/user/init.lua
@@ -1,15 +1,45 @@
 return {
-  colorscheme = "github_dark_default",
+  colorscheme = "oxocarbon",
   lsp = {
     formatting = {
       format_on_save = true,
     },
   },
+  mappings = {
+    n = {
+      ["<leader>fp"] = { "<cmd>Telescope projects<cr>" },
+      ["<leader>ln"] = { "<cmd>AerialNavToggle<cr>" },
+    },
+  },
+  options = {
+    o = {
+      guifont = "Liga SFMono Nerd Font:h12",
+    },
+  },
   plugins = {
     "AstroNvim/astrocommunity",
-    { import = "astrocommunity.colorscheme.github-nvim-theme"},
+    { import = "astrocommunity.colorscheme.oxocarbon-nvim" },
     { import = "astrocommunity.project.project-nvim" },
     { import = "astrocommunity.motion.nvim-spider" },
     { import = "astrocommunity.motion.hop-nvim" },
+    {
+      "goolord/alpha-nvim",
+      opts = function(_, opts)
+        opts.section.header.val = {
+          "  ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣭⣿⣶⣿⣦⣼⣆         ",
+          "   ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦       ",
+          "         ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷⠄⠄⠄⠄⠻⠿⢿⣿⣧⣄     ",
+          "          ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀⠄⠢⣀⡀⠈⠙⠿⠄    ",
+          "         ⢠⣿⣿⣿⠈  ⠡⠌⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀   ",
+          "  ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘⠄ ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄  ",
+          " ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄   ",
+          "⣠⣿⠿⠛⠄⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄  ",
+          "⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇⠄⠛⠻⢷⣄ ",
+          "     ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆     ",
+          "      ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ",
+        }
+      end,
+    },
+    { "rcarriga/nvim-notify", enabled = false },
   },
 }
