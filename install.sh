836feed1ef6b4f205df785568c86bff550550982
Mac <maclong9@icloud.com>
2024-03-04 12:42:47 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+GIST_URL=https://gist.githubusercontent.com/mac-codes9/cdf1d5bea296d29bc3a22b0c8980eed0/raw/7f5e8b4c28e528dae68b8daeec0a446182ea54ec
 
 message() {
     printf "\n\e[1;37m%s\e[0m\n" "$1"
@@ -19,10 +20,26 @@ error_exit() {
 
 error_clean() {
     printf "\n\033[1;31m✘ %s, removing $HOME/.config and brew then exiting.\033[0m\n" "$1"
-
     rm -rf "$HOME"/.config /opt/homebrew "$HOME"/.ssh
-
     exit 1
+}
+
+setup_sway() {
+    rm -rf ~/.config/yabai ~/.config/skhh
+    mkdir ~/.config/sway
+    curl "$GIST_URL/alacritty-gruv.toml" > ~/.config/alacritty/alacritty.toml
+    curl "$GIST_URL/sway" > ~/.config/sway/config
+    curl https://gruvbox-wallpapers.pages.dev/wallpapers/minimalistic/gruv.jpg -o ~/Pictures/wallpaper.jpg
+}
+
+setup_hypr() {
+    rm -rf ~/.config/yabai ~/.config/skhd
+    mkdir ~/.config/hypr
+    curl "$GIST_URL/alacrity-oxo" > ~/.config/alacritty/alacritty.toml
+    curl "$GIST_URL/hypridle" > ~/.config/hypr/hypridle.conf
+    curl "$GIST_URL/hyprland" > ~/.config/hypr/hyprland.conf
+    curl "$GIST_URL/hyprlock" > ~/.config/hypr/hyprlock.conf
+    curl https://raw.githubusercontent.com/Gingeh/wallpapers/main/minimalistic/romb.png -o ~/Pictures/wallpaper.png
 }
 
 clone_configuration() {
@@ -65,12 +82,12 @@ tooling_install() {
     
     if [ "$1" = "mac" ]; then
         brew tap koekeishiya/formulae
-        brew install alacritty atuin fd gh hyperkey mas neovim orbstack ripgrep skhd utm yabai
+        brew install alacritty atuin fd gh hyperkey mas neovim orbstack ripgrep sd skhd utm yabai
         mas install 1436953057
         astro_install
     elif [ "$1" = "fedora" ]; then
         sudo dnf copr enable alternateved/keyd solopasha/hyprland sramanujam/atuin
-        sudo dnf install atuin aylurs-gtk-shell alacritty fd-find gh grim keyd neovim ripgrep slurp
+        sudo dnf install atuin aylurs-gtk-shell alacritty fd-find gh grim keyd neovim ripgrep sd slurp
 
         if [ "$window_manager" = "hyprland" ]; then
             sudo dnf install hyprland-git hypridle hyprlock swww xdg-desktop-portal-hyprland
@@ -79,9 +96,7 @@ tooling_install() {
         fi
         
         sudo systemctl enable keyd && sudo systemctl start keyd
-        sudo curl https://gist.githubusercontent.com/mac-codes9/cdf1d5bea296d29bc3a22b0c8980eed0/raw/347f8f38a1c19a4873e98c5f28c9bdd2d67c20a8/keyd > /etc/keyd/default.conf
-        curl https://gist.githubusercontent.com/mac-codes9/cdf1d5bea296d29bc3a22b0c8980eed0/raw/9b59ca92f99f4e453866bed80869bf5def551b08/alacritty > ~/.config/alacritty/alacritty.toml
-        curl https://gruvbox-wallpapers.pages.dev/wallpapers/minimalistic/gruv.jpg -o ~/Pictures/wallpaper.jpg
+        sudo curl "$GIST_URL"/keyd > /etc/keyd/default.conf
         astro_install
     fi
     success "Tooling has been installed successfully"
