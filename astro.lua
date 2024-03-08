36f686967475c529e80001358dc35fea4a689c16
mac-codes9 <maclong9@icloud.com>
2024-03-08 20:56:04 +0000

    feat: astronvim changes
    - nix
    + ansible
    
    ~ hid tabline, showed breadcrumbs.
    
    - file_browser_telescope
    + oil
    
    + aerial (required by breadcrumbs)

--- astro.lua
+++ astro.lua
@@ -21,7 +21,11 @@ return {
 	mappings = {
 		n = {
 			["<leader>fp"] = { "<cmd>Telescope projects<cr>" },
-			["<leader>ff"] = { "<cmd>Telescope file_browser<cr>" },
+		},
+	},
+	options = {
+		opt = {
+			showtabline = 0,
 		},
 	},
 	plugins = {
@@ -34,9 +38,9 @@ return {
 		{ import = "astrocommunity.markdown-and-latex.vimtex" },
 		{ import = "astrocommunity.motion.hop-nvim" },
 		{ import = "astrocommunity.motion.nvim-spider" },
+		{ import = "astrocommunity.pack.ansible" },
 		{ import = "astrocommunity.pack.docker" },
 		{ import = "astrocommunity.pack.lua" },
-		{ import = "astrocommunity.pack.nix" },
 		{ import = "astrocommunity.pack.rust" },
 		{ import = "astrocommunity.pack.svelte" },
 		{ import = "astrocommunity.pack.tailwindcss" },
@@ -67,32 +71,45 @@ return {
 			opts = function(_, opts)
 				local status = require("astronvim.utils.status")
 
-				opts.tabline = nil
+				opts = {
+					tabline = nil,
 
-				opts.statusline = {
-					status.component.mode(),
-					status.component.git_branch(),
-					status.component.git_diff(),
-					status.component.diagnostics(),
-					status.component.fill(),
-					status.component.cmd_info(),
-					status.component.fill(),
-					status.component.nav({ percentage = false, scrollbar = false, ruler = {} }),
-					status.component.mode({ surround = { separator = "right" } }),
-				}
+					statusline = {
+						status.component.mode(),
+						status.component.git_branch(),
+						status.component.git_diff(),
+						status.component.diagnostics(),
+						status.component.fill(),
+						status.component.cmd_info(),
+						status.component.fill(),
+						status.component.nav({ percentage = false, scrollbar = false, ruler = {} }),
+						status.component.mode({ surround = { separator = "right" } }),
+					},
 
-				opts.statuscolumn = {
-					status.component.fill(),
-					status.component.numbercolumn(),
+					statuscolumn = {
+						status.component.fill(),
+						status.component.numbercolumn(),
+					},
+
+					winbar = {
+						status.component.breadcrumbs(),
+					},
 				}
 
 				return opts
 			end,
 		},
-		{ "nvim-telescope/telescope-file-browser.nvim" },
+		{
+			import = "astrocommunity.file-explorer.oil-nvim",
+			mappings = {
+				["<leader>O"] = { "<cmd>Oil --float<cr>" },
+			},
+			opts = {
+				deafult_file_explorer = true,
+			},
+		},
 		{ "rcarriga/nvim-notify", enabled = false },
 		{ "akinsho/toggleterm.nvim", enabled = false },
 		{ "nvim-neo-tree/neo-tree.nvim", enabled = false },
-		{ "stevearc/aerial.nvim", enabled = false },
 	},
 }
