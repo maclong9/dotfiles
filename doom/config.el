17e7f75eab634aa5aeae725af478e502753bc042
mac-codes9 <maclong9@icloud.com>
2024-03-08 12:25:08 +0000

    feat: doom-emacs configured

---
+++ doom/config.el
@@ -0,0 +1,9 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+(setq
+ user-full-name "Mac"
+ user-mail-address "maclong9@icloud.com"
+ doom-theme 'doom-one
+ display-line-numbers-type nil
+ fancy-splash-image (concat doom-private-dir "splash.webp")
+ doom-font (font-spec :family "Liga SFMono Nerd Font" :size 12 :weight 'medium)
+)
