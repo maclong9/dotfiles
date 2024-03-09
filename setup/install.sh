bd3c3bc31056fe69535eef302af907fc3c36e823
Mac <maclong9@icloud.com>
2024-03-09 01:39:23 +0000

    Update install.sh

--- setup/install.sh
+++ setup/install.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
-if [ "$(uname -s)" ]; then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  echo "eval \$(/opt/homebrew/bin/brew shellenv)" >> "$HOME"/.zprofile
-  . "$HOME"/.zprofile
-  brew install ansible
-elif [ "$(uname -s)" ]; then
-  xbps-install ansible
-else
-  echo "Unsupported system, exiting"
-  exit 1
-fi
+case "$(uname -s)" in
+  "Darwin")
+    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | sh    echo "eval \$(/opt/homebrew/bin/brew shellenv)" >> "$HOME"/.zprofile
+    . "$HOME"/.zprofile
+    brew install ansible
+    ;;
+  "Linux")
+    sudo xbps-install ansible
+    ;;
+  "OpenBSD")
+    sudo pkg add ansible
+    ;;
+  *)
+    echo "Unsupported system, exiting"
+    ;;
+esac
 
 git clone https://github.com/mac-codes9/dotfiles ~/.config
 ansible-playbook ~/.config/manifests/initialise.yml
