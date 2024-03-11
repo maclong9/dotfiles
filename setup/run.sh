8d6caa1e886d7e4afe5d5d5d38e139210d6b991e
Mac <maclong9@icloud.com>
2024-03-11 00:38:02 +0000

    Update run.sh

--- setup/run.sh
+++ setup/run.sh
@@ -6,19 +6,16 @@ handle_error() {
 
 case "$(uname -s)" in
   "Darwin")
-    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | sh || handle_error "Failed to install Homebrew."
-    echo "eval \$($(brew --prefix)/bin/brew shellenv)" >> "$HOME/.zprofile" || handle_error "Failed to setup Homebrew environment."
-    . "$HOME/.zprofile"
-    brew install ansible || handle_error "Failed to install Ansible."
+    pip3 install ansible || handle_error "Failed to install Ansible"
     ;;
   "Linux")
-    sudo xbps-install ansible git || handle_error "Failed to install Ansible."
+    xbps-install ansible git || handle_error "Failed to install Ansible."
     ;;
   *)
     handle_error "Unsupported system"
     ;;
 esac
 
-git clone https://github.com/mac-codes9/dotfiles ~/.config || handle_error "Failed to clone the dotfiles repository."
-ansible-playbook ~/.config/manifests/initialise.yml || handle_error "Failed to execute the Ansible playbook."
+git clone https://github.com/maclong9/dotfiles ~/.config || handle_error "Failed to clone the dotfiles repository."
+ansible-playbook ~/.config/setup/initialise.yml --ask-become-password || handle_error "Failed to execute the Ansible playbook."
 exit 0
