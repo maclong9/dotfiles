871752ff9321180d821c9f064fa4d900a4c0ceff
Mac <maclong9@icloud.com>
2024-03-16 17:56:46 +0000

    fix: notification daemons require dbus-run-session <3

--- scripts/maintenance.sh
+++ scripts/maintenance.sh
@@ -1,9 +1,12 @@
+complete_message = "System maintenance complete"
 if [ "$(uname -s)" = "Darwin" ]; then
     brew update
     brew upgrade
     brew cleanup
     brew doctor
+	
 elif [ -d /etc/sv ]; then
     xbps-install -Syu
     xbps-remove -RyoO
+	notify-send $complete_message
 fi
