df3976e3fe34b7ec4b89a9db85451be9e99ca05e
Mac <maclong9@icloud.com>
2024-03-20 02:31:44 +0000

    Update maintenance.sh

--- scripts/maintenance.sh
+++ scripts/maintenance.sh
@@ -1,13 +1,4 @@
-title="Maintenance"
-complete_message="System maintenance complete"
-if [ "$(uname -s)" = "Darwin" ]; then
-    brew update
-    brew upgrade
-    brew cleanup
-    brew doctor
-    osascript -e "display notification \"$complete_message\" with title \"$title\""
-elif [ -d /etc/sv ]; then
-    xbps-install -Syu
-    xbps-remove -RyoO
-    notify-send $title $complete_message
-fi
+brew update
+brew upgrade
+brew cleanup
+brew doctor
