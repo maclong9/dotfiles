a75121d207092f64151f6aa2b2b8df5691f9755c
Mac <maclong9@icloud.com>
2024-03-20 01:50:05 +0000

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,41 +1,67 @@
 #!/bin/sh
+GREEN='\033[0;32m'
+RED='\033[0;31m' 
+BLUE='\033[0;34m' 
+NO_COLOR='\033[0m'
+CHECKMARK='\xE2\x9C\x94'
+INFO='\xE2\x84\xB9'
+CROSS='\xE2\x9D\x8C'
+
+success_message() {
+  echo -e "${GREEN}${CHECKMARK} $1${NO_COLOR}"
+}
+
+info_message() {
+  echo -e "${BLUE}${INFO} $1${NO_COLOR}"
+}
+
 handle_error() {
-  echo "An error occurred: $1, exiting"
+  echo -e "${RED}${CROSS} An error occurred: $1, exiting${NO_COLOR}"
   exit 1
 }
 
 install_homebrew() {
+  info_message "Installing Homebrew..."
   if ! command -v brew > /dev/null; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     echo "eval \"\$(/opt/homebrew/bin/brew shellenv)\"" >> /Users/mac/.zprofile
     eval "$(/opt/homebrew/bin/brew shellenv)"
   fi
+  success_message "Homebrew installed"
 }
 
 clone_configuration() {
+  info_message "Cloning configuration"
   if [ -d "$HOME/.config" ]; then
-    handle_error "~/.config already exists"
+    info_message "~/.config already exists"
   else
     git clone https://github.com/maclong9/dotfiles "$HOME/.config"
   fi
+  success_message "Configuration cloned"
 }
 
 install_tools() {
+  info_message "Installing tools..."
   brew install deno mas
   brew install --cask element hyperkey orbstack osu texifier
   mas install 1289583905 # 424390742 424389933 634148309 634159523 434290957 497799835 1289583905 
   # ^ Compressor, Final Cut Pro, Logic Pro, MainStage, Motion, Xcode, Pixelmator Pro
   open -a element hyperkey 
+  success_message "Tooling installed"
 }
 
 link_configuration() {
+   info_message "Linking configuration files..."
    ln -s "$HOME/.config/gitconfig" "$HOME/.gitconfig"
    ln -s "$HOME/.config/vimrc" "$HOME/.vimrc"
    ln -s "$HOME/.config/zshrc" "$HOME/.zshrc"
+   success_message "Configuration linked"
 }
 
 setup_cron() {
+  info_message "Setting up routine cronjob's"
   echo "0 12 * * 1 $HOME/.config/scripts/maintenance.sh" | crontab -
+  success_message "Cron tasks setup"
 }
 
 install_homebrew || handle_error "Failed to install Homebrew"
