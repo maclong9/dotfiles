e819dcd1aef0920ecae9b35a497ba6d7c6365aa9
Mac <maclong9@icloud.com>
2024-03-20 00:58:05 +0000

    Update fetch.sh

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
+# Define color variables
 YELLOW='\033[1;33m'
 BLUE='\033[1;34m'
 NC='\033[0m' # No Color
-colors=(30 31 32 33 34 35 36 37)
 
-
+# Function to print yellow box
 print_yellow_box() {
     local width=${#1}
     local line=$(printf "%-${width}s" " ")
@@ -14,7 +14,7 @@ print_yellow_box() {
 }
 
 # Retrieve system information
-. /etc/os-release /usr/lib/os-release
+source /etc/os-release /usr/lib/os-release
 HOSTNAME=$(cat /etc/hostname)
 SHELL=$(basename "$SHELL")
 WM=$XDG_CURRENT_DESKTOP
@@ -23,11 +23,15 @@ cpu=$(grep 'model name' /proc/cpuinfo | uniq | awk -F ': ' '{print $2}')
 mem_total=$(grep MemTotal /proc/meminfo | awk '{print $2}')
 mem_free=$(grep MemFree /proc/meminfo | awk '{print $2}')
 mem_used=$((mem_total - mem_free))
-for i in '/var/db/kiss/installed/*'  '/var/lib/pacman/local/[0-9a-z]*' \
-'/var/lib/dpkg/info/*.list'  '/var/db/xbps/.*'  '/var/db/pkg/*/*'; do
-	set -- $i
-	[ $# -gt 1 ] && pkgs=$# && break
+
+# Find the number of installed packages and package manager
+for i in '/var/db/kiss/installed/*' '/var/lib/pacman/local/[0-9a-z]*' \
+'/var/lib/dpkg/info/*.list' '/var/db/xbps/.*' '/var/db/pkg/*/*'; do
+    set -- $i
+    [ $# -gt 1 ] && pkgs=$# && break
 done
+
+# Determine package manager
 pkg_mgr=""
 if command -v apk &>/dev/null; then
     pkg_mgr="apk"
@@ -43,19 +47,22 @@ else
     pkg_mgr="Unknown"
 fi
 
+# Print system information
 print_yellow_box "System Information"
 echo -e "${YELLOW}$(whoami)${NC}@$(cat /etc/hostname)"
 echo -e "${BLUE}os ${NC}- $ID"
-echo -e "${BLUE}sh ${NC}- $(basename "$SHELL")"
+echo -e "${BLUE}sh ${NC}- $SHELL"
 echo -e "${BLUE}term ${NC}- $TERM"
 echo -e "${BLUE}wm ${NC}- $XDG_CURRENT_DESKTOP"
 echo -e "${BLUE}up ${NC}- $up"
 echo -e "${BLUE}cpu ${NC}- $cpu"
 echo -e "${BLUE}mem ${NC}- $mem_used/$mem_total"
 echo -e "${BLUE}kern ${NC}- $(uname -r)"
-echo -e "${BLUE}pkgs ${NC}- $pkgs($pkg_mgr)"
+echo -e "${BLUE}pkgs ${NC}- $pkgs ($pkg_mgr)"
 echo ""
+
+# Print color boxes
 for color in "${colors[@]}"; do
-	    printf "\033[0;${color}m██\033[0m"
+    printf "\033[0;${color}m██\033[0m"
 done
 printf "\n"
