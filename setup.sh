a8279f3e967566d239cf7623e1664fce7375ac05
Mac <maclong9@icloud.com>
2024-03-23 16:02:04 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -87,7 +87,7 @@ install_plugins() {
 setup_tooling() {
   message "info" "Setting up tooling"
   install_plugins "Vim" "vim/pack/plugins/start" "junegunn/fzf.vim" "lifepillar/vim-mucomplete" "mattn/emmet-vim" "sheerun/vim-polyglot" "tpope/vim-fugitive" "tpope/vim-rsi"
-  install_plugins "ZSH" "zsh/plugins" "djui/alias-tips" "Aloxaf/fzf-tab" "zsh-users/zsh-syntax-highlighting"    
+  install_plugins "ZSH" "zsh/plugins" "Aloxaf/fzf-tab" "zsh-users/zsh-syntax-highlighting"    
   message "success" "Tooling setup successfully"
 }
 
