d396a027e556cb81de271d7adb62b8e679c4a53a
Mac <maclong9@icloud.com>
2024-03-23 16:58:06 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 usage() {
   for string in "Usage: %s [-i <install_command>] [-h]\n" "$0" "Options:\n" \
-    "  -i, --install-command <install_command>   Specify the install command to be used, be sure to add sudo if required\n" \
+    "  -i, --install-command <install_command>   Specify the install command to be used\n" \
     "  -h, --help                                Display this help message\n";
   do
     printf "%s" "$string"
@@ -69,29 +69,11 @@ install_tooling() {
     "$INSTALL_COMMAND" curl
   fi
   curl -fsSL "https://deno.land/install.sh" | sh
-  curl -fsSl "https://raw.githubusercontent.com/junegunn/fzf/master/install" | sh
   message "success" "Tooling installed"
-}
-
-install_plugins() {
-  TOOL="$1"; PLUGIN_PATH="$2"; shift 2
-  message "info" "Installing $TOOL plugins..." 
-  for plugin in "$@"; do
-    git clone "https://github.com/$plugin" "$HOME/.$PLUGIN_PATH"
-  done
-  message "success" "$TOOL plugins installed"
-}
-
-setup_tooling() {
-  message "info" "Setting up tooling"
-  install_plugins "Vim" "vim/pack/plugins/start" "junegunn/fzf.vim" "lifepillar/vim-mucomplete" "sheerun/vim-polyglot" "tpope/vim-fugitive" "tpope/vim-rsi"
-  install_plugins "ZSH" "zsh/plugins" "Aloxaf/fzf-tab" "zsh-users/zsh-completions" "zsh-users/zsh-syntax-highlighting"    
-  message "success" "Tooling setup successfully"
 }
 
 install_git || message "error" "installing Git"
 clone_configuration || message "error" "cloning configuration"
 link_configuration || message "error" "linking configuration files"
 install_tooling || message "error" "installing tools"
-setup_tooling || message "error" "setting up tooling"
 message "success" "System configuration complete, enjoy."
