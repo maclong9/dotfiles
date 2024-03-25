ed3f3023308311a158c370c00362b08ca69bf74e
Mac <maclong9@icloud.com>
2024-03-25 11:02:20 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ message() {
   content="$2";
   if [ "$1" = "info" ]; then
     color="$(tput setaf 4)"
-    content="$content ..."
+    content="$content..."
   elif [ "$1" = "error" ]; then
     color="$(tput setaf 4)" 
   elif [ "$1" = "success" ]; then
