1a1c0e955085d8a33b2df3a9a588a2b27f493a49
Mac <maclong9@icloud.com>
2024-03-25 11:01:31 +0000

    chore: refactored ellipses to message decleration

--- setup.sh
+++ setup.sh
@@ -1,19 +1,15 @@
 #!/bin/sh
 message() {
   content="$2";
-  case "$1" in
-    "info")    
-      color="$(tput setaf 4)" 
-      ;;
-    "error")   
-      color="$(tput setaf 1)"
-      content="$?" 
-      ;;
-    "success") 
-      color="$(tput setaf 2)" 
-      tput cuu1 && tput el
-      ;;
-  esac
+  if [ "$1" = "info" ]; then
+    color="$(tput setaf 4)"
+    content="$content ..."
+  elif [ "$1" = "error" ]; then
+    color="$(tput setaf 4)" 
+  elif [ "$1" = "success" ]; then
+   color="$(tput setaf 2)" 
+   tput cuu1 && tput el
+  fi
   
   printf "%s%s%s\n" "$color" "$content" "$(tput sgr0)"
   
@@ -24,7 +20,7 @@ message() {
 
 install_xcli() {
   if ! command -v git > /dev/null && [ "$(uname)" = "Darwin" ]; then
-    message "info" "Installing Xcode Developer Tools..."   
+    message "info" "Installing Xcode Developer Tools"   
     xcode-select --install
     sudo xcodebuild -license accept     
     message "success" "Xcode Developer Tools installed"
@@ -33,7 +29,7 @@ install_xcli() {
 
 install_tooling() {
   if ! command -v deno > /dev/null; then
-    message "info" "Installing tools..." 
+    message "info" "Installing tool"
     curl -SLs "https://deno.land/install.sh" | sh >/dev/null 2>&1
     message "success" "Tooling installed"
   fi  
@@ -43,14 +39,14 @@ clone_configuration() {
   if [ -d "$HOME/.config" ]; then
     message "info" "$HOME/.config already exists"
   else
-    message "info" "Cloning configuration..."
+    message "info" "Cloning configuration"
     git clone -q "https://github.com/maclong9/dotfiles" "$HOME/.config"
     message "success" "Configuration cloned"
   fi
 }
 
 link_configuration() {
-   message "info" "Linking configuration files..." 
+   message "info" "Linking configuration files" 
    for file in "gitconfig" "vimrc" "zshrc"; do
      ln -s "$HOME/.config/$file" "$HOME/.$file"
    done 
