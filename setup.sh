948766ef19e9caa14bd25b88f156b48967c419b4
Mac <maclong9@icloud.com>
2024-03-29 13:59:47 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,14 @@
-#!/bin/sh
+#!/bin/sh -e
 message() {
   ANSI_PREFIX=$(printf "\033")
   if [ "$1" = "info" ]; then
     printf "%s" "${ANSI_PREFIX}[34m" # Sets Foreground to Blue
-  elif [ "$1" = "error" ]; then
-    printf "%s" "${ANSI_PREFIX}[31m" # Sets Foreground to Red
   elif [ "$1" = "success" ]; then
     printf "%s" "${ANSI_PREFIX}[32m" # Sets Foreground to Green
     printf "%s[1A%s[K" "${ANSI_PREFIX}" "${ANSI_PREFIX}" # Clears Previous Line
   fi
   
   printf "%s%s[0m\n" "$2" "${ANSI_PREFIX}" # Prints message & clears previous Foreground color
-  
-  if [ "$1" = "error" ]; then
-    exit 1
-  fi
 }
 
 install_xcli() {
@@ -38,7 +32,7 @@ clone_configuration() {
   if [ -d "$HOME/.config" ]; then
     message "info" "$HOME/.config already exists"
   else
-    message "info" "Cloning configuration..."
+		message "info" "Cloning configuration..."
     git clone -q "https://github.com/maclong9/dotfiles" "$HOME/.config"
     message "success" "Configuration cloned"
   fi
@@ -52,8 +46,9 @@ link_configuration() {
    message "success" "Configuration linked"
 }
 
-install_xcli || message "error"
-install_tooling || message "error"
-clone_configuration || message "error"
-link_configuration || message "error"
+message "info" "System Initialising"
+install_xcli 
+install_tooling 
+clone_configuration
+link_configuration 
 message "success" "System configuration complete, enjoy."
