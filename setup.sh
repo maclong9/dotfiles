05af51ace64385cc07a332eca2b08de9db6c42e0
Mac <maclong9@icloud.com>
2024-03-30 12:46:50 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -e
 message() {
   ansi_pre=$(printf "\033")
-  [ "$1" = "info" ] && color="${ansi_pre}[34m" # Foreground > Blue
-  [ "$1" = "success" ] && color="${ansi_pre}[1A${ansi_pre}[K${ansi_pre}[32m" # Deletes Previous Line & Foreground > Green
-  [ "$1" = "error" ] && color="${ansi_pre}[31m" # Foreground > Red
+  [ "$1" = "info" ] && color="${ansi_pre}[34m" # Blue
+  [ "$1" = "success" ] && color="${ansi_pre}[1A${ansi_pre}[K${ansi_pre}[32m" # Clear ^ & Green
+  [ "$1" = "error" ] && color="${ansi_pre}[31m" # Red
 
   printf "%s[%s] %s%s\n" "$color" "$1" "${ansi_pre}[0m" "$2"
 }
