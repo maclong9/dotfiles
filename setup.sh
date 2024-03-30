560d06b337449d77807b669f3a4a7271c8968098
Mac <maclong9@icloud.com>
2024-03-30 00:23:21 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,12 +3,10 @@ message() {
   ANSI_PREFIX=$(printf "\033")
   if [ "$1" = "info" ]; then
     color="${ANSI_PREFIX}[34m" # Sets Foreground to Blue
-  elif [ "$1" = "info" ]; then
-    color="${ANSI_PREFIX}[31m" # Sets Foreground to Red
   elif [ "$1" = "success" ]; then
     color="${ANSI_PREFIX}[1A ${ANSI_PREFIX}[K ${ANSI_PREFIX}[32m" # Clears previous line & sets Foreground to Green
   fi
-  
+
   printf "%s[%s] %s%s\n" "$color" "$1" "${ANSI_PREFIX}[0m" "$2"
 }
 
