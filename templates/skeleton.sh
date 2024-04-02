4340bdd813955bf83d7ff9810076719966acd95a
Mac <maclong9@icloud.com>
2024-04-02 14:29:42 +0100

    fix(templates/sh): added exit on error and " -> '

--- templates/skeleton.sh
+++ templates/skeleton.sh
@@ -5,5 +5,7 @@ message() {
   [ "$1" = "success" ] && color="${ansi_pre}[1A${ansi_pre}[K${ansi_pre}[32m" # Clear ^ & Green
   [ "$1" = "error" ] && color="${ansi_pre}[31m" # Red
 
-  printf "%s[%s] %s%s\n" "$color" "$1" "${ansi_pre}[0m" "$2"
+  printf '%s[%s] %s%s\n' "$color" "$1" "${ansi_pre}[0m" "$2"
+
+	[ "$1" = "error" ] && return 1
 }
