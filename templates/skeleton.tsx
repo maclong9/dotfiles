90834651d7662dd3579421573a04df8601237573
Mac <maclong9@icloud.com>
2024-04-05 19:03:42 +0100

    Create skeleton.tsx

---
+++ templates/skeleton.tsx
@@ -0,0 +1,17 @@
+import _ from "";
+
+interface Props {
+  // Define your component props here
+}
+
+const MyComponent: PrReact.FC<Props> = ({ /* prop values */ }) => {
+  // Component logic and state management goes here
+
+  return (
+    <div>
+      {/* Render your component */}
+    </div>
+  );
+};
+
+export default MyComponent;
