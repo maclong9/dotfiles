b5aa0e15b9fbc46e64cc4ea8b27dcfbbd18e7ab4
Mac <maclong9@icloud.com>
2024-05-30 18:22:20 +0100

    feat: converted setup script to swift

---
+++ setup.swift
@@ -0,0 +1,48 @@
+#!/usr/bin/swift
+import Foundation
+
+extension Process {
+	private static let gitExecPath = URL(fileURLWithPath: "/usr/bin/git")
+	private static let lnExecPath = URL(fileURLWithPath: "/bin/ln")
+	
+	public func clone(repo: String, path: String) throws {
+		executableURL = Process.gitExecPath
+		arguments = ["clone", "-q", repo, path]
+		try run()
+	}
+	
+	public func link(src: URL, dest: URL) throws {
+		executableURL = Process.lnExecPath
+		arguments = ["-s", src.path, dest.path] // Convert URLs to strings
+		try run()
+	}
+}
+
+var configPath = "/Users/mac/testing123"
+
+do {
+	try Process().clone(
+		repo: "https://github.com/maclong9/dotfiles",
+		path: configPath
+	)
+	
+	sleep(4)
+	
+	let enumerator = FileManager.default.enumerator(
+		at: URL(fileURLWithPath: configPath),
+		includingPropertiesForKeys: [.isRegularFileKey],
+		options: [.skipsHiddenFiles, .skipsPackageDescendants]
+	)
+	
+	while let fileUrl = enumerator?.nextObject() as? URL {
+		if fileUrl.hasDirectoryPath == false {
+			let destUrl = URL(fileURLWithPath: "/Users/mac/.\(fileUrl.lastPathComponent)")
+			try Process().link(
+				src: fileUrl, // Pass URL objects directly
+				dest: destUrl
+			)
+		}
+	}
+} catch {
+	print("Error: \(error)")
+}
