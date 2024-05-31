e230b5d2628f64413268d257ac986c9be7318c90
Mac <maclong9@icloud.com>
2024-05-31 10:32:42 +0100

    Update setup.swift

--- setup.swift
+++ setup.swift
@@ -2,48 +2,48 @@
 import Foundation
 
 extension Process {
-	private static let gitExecPath = URL(fileURLWithPath: "/usr/bin/git")
-	private static let lnExecPath = URL(fileURLWithPath: "/bin/ln")
-	
-	public func clone(repo: String, path: String) throws {
-		executableURL = Process.gitExecPath
-		arguments = ["clone", "-q", repo, path]
-		try run()
-	}
-	
-	public func link(src: URL, dest: URL) throws {
-		executableURL = Process.lnExecPath
-		arguments = ["-s", src.path, dest.path]
-		try run()
-	}
+  private static let gitExecPath = URL(fileURLWithPath: "/usr/bin/git")
+  private static let lnExecPath = URL(fileURLWithPath: "/bin/ln")
+
+  public func clone(repo: String, path: String) throws {
+    executableURL = Process.gitExecPath
+    arguments = ["clone", "-q", repo, path]
+    try run()
+  }
+
+  public func link(src: URL, dest: URL) throws {
+    executableURL = Process.lnExecPath
+    arguments = ["-s", src.path, dest.path]
+    try run()
+  }
 }
 
 do {
-	let configPath = "/Users/mac/.config"
-	
-	try Process().clone(
-		repo: "https://github.com/maclong9/dotfiles",
-		path: configPath
-	)
-	
-	sleep(4)
-	
-	let enumerator = FileManager.default.enumerator(
-		at: URL(fileURLWithPath: configPath),
-		includingPropertiesForKeys: [.isRegularFileKey],
-		options: [.skipsHiddenFiles, .skipsPackageDescendants]
-	)
-	
-	while let fileUrl = enumerator?.nextObject() as? URL {
-		fileUrl.pathExtension != "swift" {
-			try Process().link(
-				src: fileUrl,
-				dest: URL(
-					fileURLWithPath: "/Users/mac/.\(fileUrl.lastPathComponent)"
-				)
-			)
-		}
-	}
+  let configPath = "/Users/mac/.config"
+
+  try Process().clone(
+    repo: "https://github.com/maclong9/dotfiles",
+    path: configPath
+  )
+
+  sleep(4)
+
+  let enumerator = FileManager.default.enumerator(
+    at: URL(fileURLWithPath: configPath),
+    includingPropertiesForKeys: [.isRegularFileKey],
+    options: [.skipsHiddenFiles, .skipsPackageDescendants]
+  )
+
+  while let fileUrl = enumerator?.nextObject() as? URL {
+    if fileUrl.pathExtension != "swift" {
+      try Process().link(
+        src: fileUrl,
+        dest: URL(
+          fileURLWithPath: "/Users/mac/.\(fileUrl.lastPathComponent)"
+        )
+      )
+    }
+  }
 } catch {
-	print("Error: \(error)")
+  print("Error: \(error)")
 }
