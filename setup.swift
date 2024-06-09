2429ceda62090297651dcd734e12bc863cc93b5e
Mac <maclong9@icloud.com>
2024-06-09 13:16:44 +0100

    Update setup.swift

--- setup.swift
+++ setup.swift
@@ -19,18 +19,24 @@ extension Process {
     waitUntilExit()
   }
 
-  public func install(from src: String) throws {
+  func install(from src: String) throws {
     executableURL = URL(fileURLWithPath: "/bin/sh")
     arguments = ["-c", "curl \(src) | sh"]
     try run()
     waitUntilExit()
   }
+
+  public func installTooling() throws {  
+    try Process().install(from: "https://deno.land/install.sh")
+    try Process().install(from:
+      "https://gist.githubusercontent.com/maclong9/32616842c8197da8271dda426b78f87c/raw/147a8a49194f3cc18ecc58578226996001adef8c/install-tmux.sh"
+    )
+    try FileManager.default.removeItem(at: URL(fileURLWithPath: "\(homeDir)/tmux-install"))
+  }
 }
 
 do {
   try Process().clone(from: "https://github.com/maclong9/dotfiles", to: configPath)
-
-  sleep(4)
 
   let enumerator = FileManager.default.enumerator(
     at: URL(fileURLWithPath: configPath),
@@ -46,13 +52,8 @@ do {
       )
     }
   }
-  try Process().install(from: "https://deno.land/install.sh")
 
-  try Process().install(
-    from:
-      "https://gist.githubusercontent.com/maclong9/32616842c8197da8271dda426b78f87c/raw/147a8a49194f3cc18ecc58578226996001adef8c/install-tmux.sh"
-  )
-  try FileManager.default.removeItem(at: URL(fileURLWithPath: "\(homeDir)/tmux-install"))
+  try Process().installTooling()
 } catch {
   print("Error: \(error)")
 }
