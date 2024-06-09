992d311459b72b3a73d0f62183556c6320d02cfa
Mac <maclong9@icloud.com>
2024-06-09 10:34:41 +0100

    Update setup.swift

--- setup.swift
+++ setup.swift
@@ -5,29 +5,29 @@ extension Process {
   private static let gitExecPath = URL(fileURLWithPath: "/usr/bin/git")
   private static let lnExecPath = URL(fileURLWithPath: "/bin/ln")
   private static let shExecPath = URL(fileURLWithPath: "/bin/sh")
-
+  
   public func clone(from repo: String, to path: String) throws {
     executableURL = Process.gitExecPath
     arguments = ["clone", "-q", repo, path]
     try run()
     waitUntilExit()
   }
-
+  
   public func link(from src: URL, to dest: URL) throws {
     executableURL = Process.lnExecPath
     arguments = ["-s", src.path, dest.path]
     try run()
     waitUntilExit()
   }
-
-  public func install(from src: String, withOutput: Bool = false, extraArgs: String? = nil) throws {
+  
+  public func install(from src: String, withOutput: Bool = false, extraArgs: String? = nil, runAfter: String? = nil) throws {
     executableURL = Process.shExecPath
     arguments = ["-c", "curl -fsSL \(withOutput ? "-o" : "") \(extraArgs ?? "")  \(src) | sh"]
     try run()
     waitUntilExit()
-
-    if let runAfterCommand = runAfter {
-      arguments = ["-c", runAfterCommand]
+    
+    if let runAfter = runAfter {
+      arguments = ["-c", runAfter]
       try run()
       waitUntilExit()
     }
@@ -41,15 +41,15 @@ do {
     from: "https://github.com/maclong9/dotfiles",
     to: configPath
   )
-
+  
   sleep(4)
-
+  
   let enumerator = FileManager.default.enumerator(
     at: URL(fileURLWithPath: configPath),
     includingPropertiesForKeys: [.isRegularFileKey],
     options: [.skipsHiddenFiles, .skipsPackageDescendants]
   )
-
+  
   while let fileUrl = enumerator?.nextObject() as? URL {
     if fileUrl.pathExtension != "swift" {
       try Process().link(
@@ -60,19 +60,19 @@ do {
       )
     }
   }
-
+  
   if !FileManager.default.fileExists(atPath: "\(homeDir)/.deno") {
     try Process().install(from: "https://deno.land/install.sh")
   }
-
-  if !FileManager.default.fileExists(atPath: URL(fileUrlWithPath: "/usr/local/bin/tmux")) {
+  
+  if !FileManager.default.fileExists(atPath: URL(fileURLWithPath: "/usr/local/bin/tmux").path) {
     try Process().install(
       from:
         "https://gist.githubusercontent.com/tomasbasham/1e405cfa16e88c0f5d2f49bbbd161944/raw/c70c143eecadc3ca67317227bbb687f51486353d/install_tmux_osx_no_brew",
       runAfter: "rm -rf \(homeDir)/tmux-install"
     )
   }
-
+  
   if !FileManager.default.fileExists(atPath: "\(homeDir)/.vim") {
     try Process().install(
       from: "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim",
