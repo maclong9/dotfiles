65a15f256a15eb935b08857a4c278158c5de8045
Mac <maclong9@icloud.com>
2024-06-10 12:50:03 +0100

    Update setup.swift

--- setup.swift
+++ setup.swift
@@ -54,7 +54,7 @@ do {
   )
 
   while let fileUrl = enumerator?.nextObject() as? URL {
-    if fileUrl.pathExtension.isEmpty {
+    if fileUrl.pathExtension.isEmpty || fileUrl.pathExtension.contains("conf") {
       try Process().link(
         from: fileUrl.path,
         to: "\(homeDir)/.\(fileUrl.lastPathComponent)"
