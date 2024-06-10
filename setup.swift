1bc16ddaa37267504fad5756d20cc7b3e5801313
Mac <maclong9@icloud.com>
2024-06-10 12:43:14 +0100

    Update setup.swift

--- setup.swift
+++ setup.swift
@@ -6,61 +6,65 @@ let configPath = "\(homeDir)/.config"
 let mintPath = "\(homeDir)/Mint"
 
 extension Process {
+  public func execute(_ binary: String, with args: [String]? = []) throws {
+    executableURL = URL(fileURLWithPath: binary)
+    arguments = args
+    try run()
+    waitUntilExit()
+  }
+
   public func clone(from repo: String, to path: String) throws {
-    executableURL = URL(fileURLWithPath: "/usr/bin/git")
-    arguments = ["clone", "-q", repo, path]
-    try run()
-    waitUntilExit()
+    try execute(
+      "/usr/bin/git",
+      with: [
+        "clone",
+        "-q",
+        repo.contains("github") ? repo : "https://github.com/\(repo)",
+        path,
+      ])
   }
 
-  public func link(from src: URL, to dest: URL) throws {
-    executableURL = URL(fileURLWithPath: "/bin/ln")
-    arguments = ["-s", src.path, dest.path]
-    try run()
-    waitUntilExit()
+  public func link(from src: String, to dest: String) throws {
+    try execute("/bin/ln", with: ["-s", src, dest])
   }
 
-  func install(from src: String) throws {
-    executableURL = URL(fileURLWithPath: "/bin/sh")
-    arguments = ["-c", "curl -fsSL \(src) | sh"]
-    try run()
-    waitUntilExit()
+  func install(from url: URL, install: Bool? = false) throws {
+    if !url.path.contains("github") {
+      try Process().clone(from: url.path, to: url.lastPathComponent)
+    }
+
+    FileManager.default.changeCurrentDirectoryPath(url.lastPathComponent)
+
+    if let install = install, !install {
+        try execute("/usr/bin/sudo", with: ["make"])
+    }
   }
 
-  func runCommand(at path: String? = FileManager.default.currentDirectoryPath, with args: [String]) throws {
-    executableURL = URL(fileURLWithPath: "/usr/bin/env")
-    arguments = ["sh", "-c", path != nil ? "cd \(path!) && \(args.joined(separator: " "))" : args.joined(separator: " ")]
-    try run()
-    waitUntilExit()
+  func installFromScript(from src: String) throws {
+    try execute("/bin/sh", with: ["-c", "curl", "-fsSl", src, "| sh"])
   }
 }
 
 do {
-  try Process().clone(from: "https://github.com/maclong9/dotfiles", to: configPath)
-
+  try Process().clone(from: "maclong9/dotfiles", to: configPath)
   let enumerator = FileManager.default.enumerator(
     at: URL(fileURLWithPath: configPath),
     includingPropertiesForKeys: [.isRegularFileKey],
-    options: [.skipsHiddenFiles, .skipsPackageDescendants]
+    options: [.skipsHiddenFiles]
   )
 
   while let fileUrl = enumerator?.nextObject() as? URL {
-    if fileUrl.pathExtension != "swift" && fileUrl.pathExtension != "md" {
+    if !fileUrl.pathExtension.isEmpty {
       try Process().link(
-        from: fileUrl,
-        to: URL(fileURLWithPath: "\(homeDir)/.\(fileUrl.lastPathComponent)")
+        from: fileUrl.path,
+        to: "\(homeDir)/.\(fileUrl.lastPathComponent)"
       )
     }
   }
 
-  try Process().install(from: "https://deno.land/install.sh")
-  try Process().install(
-    from: "https://gist.githubusercontent.com/maclong9/32616842c8197da8271dda426b78f87c/raw/147a8a49194f3cc18ecc58578226996001adef8c/install-tmux.sh"
-  )
-  try FileManager.default.removeItem(at: URL(fileURLWithPath: "\(homeDir)/tmux-install"))
-  try Process().clone(from: "https://github.com/yonaskolb/Mint.git", to: mintPath)
-  try Process().runCommand(at: mintPath, with: ["sudo make"])
-  try Process().runCommand(with: ["mint install maclong9/SwiftList"])
+  try Process().installFromScript(from: "https://deno.land/install.sh")
+  try Process().install(from: URL(string: "https://github.com/yonaskolb/Mint")!)
 } catch {
   print("Error: \(error)")
 }
+
