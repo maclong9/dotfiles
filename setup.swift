00d4ce2e17a5e0f3b2b0ae25a6b0d70d1601ac21
Mac <maclong9@icloud.com>
2024-06-11 10:52:08 +0100

    Update setup.swift

--- setup.swift
+++ setup.swift
@@ -6,50 +6,50 @@ let configPath = "\(homeDir)/.config"
 let fm = FileManager.default
 
 extension Process {
-  public func execute(_ binary: String, with args: [String]) throws {
-    executableURL = URL(fileURLWithPath: binary)
-    arguments = args
-    try run()
-    waitUntilExit()
-  }
+    public func execute(_ binary: String, with args: [String]) throws {
+        executableURL = URL(fileURLWithPath: binary)
+        arguments = args
+        try run()
+        waitUntilExit()
+    }
 }
 
 do {
-  try Process().execute(
-    "/usr/bin/git", 
-    with: [
-      "clone", 
-      "-q", 
-      "https://github.com/maclong9/dotfiles", 
-      configPath
-    ]
-  )
-  try Process().execute(
-    "/bin/sh", 
-    with: [
-      "-c", 
-      "curl -fsSL https://deno.land/install.sh | sh"
-    ]
-  )
-
-  let enumerator = fm.enumerator(
-    at: URL(fileURLWithPath: configPath),
-    includingPropertiesForKeys: [.isRegularFileKey],
-    options: [.skipsHiddenFiles]
-  )
-
-  while let fileUrl = enumerator?.nextObject() as? URL {
-    if fileUrl.pathExtension.isEmpty {
-      try Process().execute(
-        "/bin/ln",
+    try Process().execute(
+        "/usr/bin/git",
         with: [
-          "-s",
-          fileUrl.path,
-          "\(homeDir)/.\(fileUrl.lastPathComponent)",
+            "clone",
+            "-q",
+            "https://github.com/maclong9/dotfiles",
+            configPath
         ]
-      )
+    )
+    try Process().execute(
+        "/bin/sh",
+        with: [
+            "-c",
+            "curl -fsSL https://deno.land/install.sh | sh"
+        ]
+    )
+
+    let enumerator = fm.enumerator(
+        at: URL(fileURLWithPath: configPath),
+        includingPropertiesForKeys: [.isRegularFileKey],
+        options: [.skipsHiddenFiles]
+    )
+
+    while let fileUrl = enumerator?.nextObject() as? URL {
+        if fileUrl.pathExtension.isEmpty {
+            try Process().execute(
+                "/bin/ln",
+                with: [
+                    "-s",
+                    fileUrl.path,
+                    "\(homeDir)/.\(fileUrl.lastPathComponent)",
+                ]
+            )
+        }
     }
-  }
 } catch {
-  print("Error: \(error)")
+    print("Error: \(error)")
 }
