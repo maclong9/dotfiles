4ab188a6ab4d2e5062fc6c1c6c96e7de732100af
Mac <maclong9@icloud.com>
2024-07-26 16:42:01 +0100

    fix: removed error-lens as it's there already
    Signed-off-by: Mac <maclong9@icloud.com>

--- nvim/init.lua
+++ nvim/init.lua
@@ -512,16 +512,6 @@ require("lazy").setup({
 		end,
 	},
 	{
-		"chikko80/error-lens.nvim",
-		event = "BufRead",
-		dependencies = {
-			"nvim-telescope/telescope.nvim",
-		},
-		config = function()
-			require("error-lens").setup()
-		end,
-	},
-	{
 		"nvim-neotest/neotest",
 		dependencies = {
 			"nvim-neotest/nvim-nio",
