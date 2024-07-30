fb5b873aa2b66cf084b346630fc9ddf6af6074de
Mac <maclong9@icloud.com>
2024-07-30 23:44:16 +0100

    feat: updated to lazynvim

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,18 +1,198 @@
 return {
-  { "lunacookies/vim-colors-xcode" },
+  "lunacookies/vim-colors-xcode",
+  "tpope/vim-rsi",
+  "stevearc/dressing.nvim",
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "xcodedark",
+      colorscheme = "xcode",
     },
     init = function()
       vim.opt.termguicolors = false
-      vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
-      vim.cmd("hi EndOfBuffer guibg=NONE ctermbg=NONE")
-      vim.cmd("hi StatusLine guibg=NONE guifg=#666666 ctermbg=NONE ctermfg=244")
-      vim.cmd("hi LineNr guifg=#666666 ctermfg=244")
-      vim.cmd("hi Comment guifg=#666666 ctermfg=244")
+    end,
+  },
+
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+
+  {
+    "stevearc/oil.nvim",
+    opts = {
+      default_file_explorer = true,
+      view_options = {
+        show_hidden = true,
+      },
+    },
+    event = "VeryLazy",
+    keys = {
+      { "<leader>e", "<cmd>Oil<cr>", desc = "Open parent directory" },
+    },
+    config = function()
+      require("oil").setup()
+    end,
+  },
+
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    config = function()
+      local lualine = require("lualine")
+
+      local colors = {
+        bg = "#202328",
+        fg = "#bbc2cf",
+        yellow = "#ECBE7B",
+        cyan = "#008080",
+        darkblue = "#081633",
+        green = "#98be65",
+        orange = "#FF8800",
+        violet = "#a9a1e1",
+        magenta = "#c678dd",
+        blue = "#51afef",
+        red = "#ec5f67",
+      }
+
+      local conditions = {
+        buffer_not_empty = function()
+          return vim.fn.empty(vim.fn.expand("%:t")) ~= 1
+        end,
+        hide_in_width = function()
+          return vim.fn.winwidth(0) > 80
+        end,
+        check_git_workspace = function()
+          local filepath = vim.fn.expand("%:p:h")
+          local gitdir = vim.fn.finddir(".git", filepath .. ";")
+          return gitdir and #gitdir > 0 and #gitdir < #filepath
+        end,
+      }
+
+      -- Config
+      local config = {
+        options = {
+          component_separators = "",
+          section_separators = "",
+          theme = {
+            normal = { c = { fg = colors.fg, bg = colors.bg } },
+            inactive = { c = { fg = colors.fg, bg = colors.bg } },
+          },
+        },
+        sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_y = {},
+          lualine_z = {},
+          lualine_c = {},
+          lualine_x = {},
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_y = {},
+          lualine_z = {},
+          lualine_c = {},
+          lualine_x = {},
+        },
+      }
+
+      local function ins_left(component)
+        table.insert(config.sections.lualine_c, component)
+      end
+
+      local function ins_right(component)
+        table.insert(config.sections.lualine_x, component)
+      end
+
+      ins_left({
+        function()
+          return "▊"
+        end,
+        color = { fg = colors.blue },
+        padding = { left = 0, right = 1 },
+      })
+
+      ins_left({
+        function()
+          return ""
+        end,
+        color = function()
+          local mode_color = {
+            n = colors.red,
+            i = colors.green,
+            v = colors.blue,
+            [""] = colors.blue,
+            V = colors.blue,
+            c = colors.magenta,
+            no = colors.red,
+            s = colors.orange,
+            S = colors.orange,
+            [""] = colors.orange,
+            ic = colors.yellow,
+            R = colors.violet,
+            Rv = colors.violet,
+            cv = colors.red,
+            ce = colors.red,
+            r = colors.cyan,
+            rm = colors.cyan,
+            ["r?"] = colors.cyan,
+            ["!"] = colors.red,
+            t = colors.red,
+          }
+          return { fg = mode_color[vim.fn.mode()] }
+        end,
+        padding = { right = 1 },
+      })
+
+      ins_left({
+        "filename",
+        cond = conditions.buffer_not_empty,
+        color = { fg = colors.magenta, gui = "bold" },
+      })
+
+      ins_left({ "location" })
+
+      ins_left({
+        "diagnostics",
+        sources = { "nvim_diagnostic" },
+        symbols = { error = " ", warn = " ", info = " " },
+        diagnostics_color = {
+          error = { fg = colors.red },
+          warn = { fg = colors.yellow },
+          info = { fg = colors.cyan },
+        },
+      })
+
+      ins_left({
+        function()
+          return "%="
+        end,
+      })
+
+      ins_right({
+        "branch",
+        icon = "",
+        color = { fg = colors.violet, gui = "bold" },
+      })
+
+      ins_right({
+        "diff",
+        symbols = { added = " ", modified = "󰝤 ", removed = " " },
+        diff_color = {
+          added = { fg = colors.green },
+          modified = { fg = colors.orange },
+          removed = { fg = colors.red },
+        },
+        cond = conditions.hide_in_width,
+      })
+
+      ins_right({
+        function()
+          return "▊"
+        end,
+        color = { fg = colors.blue },
+        padding = { left = 1 },
+      })
+
+      lualine.setup(config)
     end,
   },
 }
